75d58993ced5d68523154f5cc6cae2116ab6a327
podvistorcheto <denev.s.deyan@gmail.com>
2020-11-10 12:20:25 +0000

    07-User Profile and Default Picture

--- football_report/urls.py
+++ football_report/urls.py
@@ -16,11 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('accounts/', include('allauth.urls')),
     path('profile/', include('profiles.urls')),
-    #path('', include('homepage.urls')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- profiles/models.py
+++ profiles/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class CreateProfile(models.Model):
+class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default="default_image.jpg", upload_to="profile_imgs")
+    image = models.ImageField(default="default.jpg", upload_to="profile_pics")
 
     def __str__(self):
-        return f'{self.user.username} Hello traveller!'
+        return f'{self.user.username} Profile'
