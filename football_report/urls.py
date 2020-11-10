372de3d1db0bcf5ebee4f21ad9a36f6c51cd63d2
podvistorcheto <denev.s.deyan@gmail.com>
2020-11-10 17:56:31 +0000

    08-Default Picture bug fixed

--- football_report/urls.py
+++ football_report/urls.py
@@ -18,6 +18,8 @@ from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from profiles import views as user_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,7 +27,6 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('profile/', include('profiles.urls')),
 ]
-
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- profiles/signals.py
+++ profiles/signals.py
@@ -1,15 +1,7 @@
-from django.db import models
-from django.contrib.auth.models import User
 from django.db.models.signals import post_save
-
+from django.contrib.auth.models import User
 from django.dispatch import receiver
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default="default.jpg", upload_to="profile_pics")
-
-    def __str__(self):
-        return f'{self.user.username} Profile'
+from .models import Profile
 
 
 @receiver(post_save, sender=User)
