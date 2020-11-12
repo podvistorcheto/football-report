2a0fbe5176f19bcf87a632407c7e07aeab27d53c
podvistorcheto <denev.s.deyan@gmail.com>
2020-11-12 17:38:06 +0000

    18-Added forms for the checkout

---
+++ checkout/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Package
+
+
+class PackageForm(forms.ModelForm):
+    class Meta:
+        model = Package
+        fields = (
+            'full_name',
+            'phone_number',)
