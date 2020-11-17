efab37d97eede24038d00c144f6bbde9c8c35203
podvistorcheto <denev.s.deyan@gmail.com>
2020-11-17 16:30:24 +0000

    45-Updated image styles

--- static/js/base.js
+++ static/js/base.js
@@ -30,14 +30,14 @@
       });
     });
     //Get the go to top button
-    var mybutton = document.getElementById("myBtn");
+    var mybutton = document.getElementById("#myBtn");
     // When the user scrolls down 20px from the top of the document, show the button
     window.onscroll = function() {scrollFunction()};
     function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        style.display = "block";
+        display = "block";
     } else {
-        style.display = "none";
+        display = "none";
     }
     }
     // When the user clicks on the button, scroll to the top of the document
